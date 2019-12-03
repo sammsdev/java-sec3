ce04d1783255c8f5deeba977f14e80a768706abb
Samuel Magalhaes Silva <sammsdev@gmail.com>
2019-12-02 22:26:49 -0300

    ByteShortIntLong

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,33 @@
 public class HelloWorld {
 
     public static void main (String [] args){
-        System.out.printf("Hello World!");
+        int myFirstNumber = 5;
+        System.out.println("Hello World! First Number is = " + myFirstNumber);
+
+        /*  byteShortIntLong  */
+        
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte Minimum Value = " + myMinByteValue);
+        System.out.println("Byte Maximum Value = " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short Minimum Value = " + myMinShortValue);
+        System.out.println("Short Maximum Value = " + myMaxShortValue);
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximum Value = " + myMaxIntValue);
+
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long Minimum Value = " + myMinLongValue);
+        System.out.println("Long Maximum Value = " + myMaxLongValue);
+
+        /* end byteShortIntLong  */
+
     }
 
 }

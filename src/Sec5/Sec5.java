e27ea0f842deebb49fa8a39a3bc13d3ab58df866
Samuel Magalhaes Silva <sammsdev@gmail.com>
2019-12-07 16:27:56 -0300

    Section 5 Done

--- src/Sec5/Sec5.java
+++ src/Sec5/Sec5.java
@@ -1,11 +1,13 @@
 package Sec5;
 
+import java.util.Scanner;
+
 public class Sec5 {
 
     public static void main(String[] args) {
 
         // System.out.println(numberToWords(123));
-        numberToWords(-2222);
+        inputThenPrintSumAndAverage();
     }
 
     public static void printDayOfTheWeek(int day) {
@@ -130,7 +132,7 @@ public class Sec5 {
     public static int getDigitCount(int number) {
         if (number < 0)
             return -1;
-        if(number == 0)
+        if (number == 0)
             return 1;
         int c = 0;
         while (number > 0) {
@@ -156,9 +158,9 @@ public class Sec5 {
         int lastDigit = 0;
         int correctOrderNum = reverse(number);
         int dif = (getDigitCount(number) - getDigitCount(correctOrderNum));
-        if(correctOrderNum == 0)
+        if (correctOrderNum == 0)
             System.out.print("Zero ");
-        if(getDigitCount(number) == -1)
+        if (getDigitCount(number) == -1)
             System.out.println("Invalid Value");
 
         while (correctOrderNum > 0) {
@@ -201,6 +203,27 @@ public class Sec5 {
             for (int i = 0; i < dif; i++)
                 System.out.print("Zero ");
         }
+
+    }
+
+    public static void inputThenPrintSumAndAverage() {
+        int sum = 0, c = 0;
+        long avg = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            boolean isAnInt = scanner.hasNextInt();
+            if (isAnInt) {
+                sum+= scanner.nextInt();
+                c++;
+                avg = Math.round((sum/c ));
+            } else {
+                break;
+            }
+            scanner.nextLine();
+        }
+        System.out.println("SUM = " + sum + " AVG = " + avg);
+        scanner.close();
 
     }
 
